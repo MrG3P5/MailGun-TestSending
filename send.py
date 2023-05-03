3c0dbb4928dc05a2b0016a1c9be3290adef36cd2
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2023-05-03 10:42:30 +0700

    change
    change

--- send.py
+++ send.py
@@ -1,12 +1,11 @@
 import requests
 import sys
 
-YOUR_MAIL_HERE = "example@gmail.com"
-
 try:
     key = sys.argv[1]
     domain = sys.argv[2]
-    req = requests.post(f"https://api.mailgun.net/v3/{domain}/messages", auth=("api", f"{key}"), data={"from": f"Support <mailgun@{domain}>", "to": [YOUR_MAIL_HERE], "subject": "Test mailgun", "text": "Testing mailgun"})
+    to_mail = sys.argv[3]
+    req = requests.post(f"https://api.mailgun.net/v3/{domain}/messages", auth=("api", f"{key}"), data={"from": f"Support <mailgun@{domain}>", "to": [to_mail], "subject": "Test mailgun", "text": "Testing mailgun"})
     print(req.json())
 except:
-    print(f"Usage : python3 {sys.argv[0]} <Key> <Domain>")
+    print(f"Usage : python3 {sys.argv[0]} <Key> <Domain> <To_Mail>")
